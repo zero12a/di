603f2cecbeba5f32c4f78094c1805a080a0aa7b9
kims man <zero12a@gmail.com>
2019-12-09 16:02:23 +0900

    s

--- msacg/entrypoint.sh
+++ msacg/entrypoint.sh
@@ -4,10 +4,5 @@ printf "\n\nStarting PHP daemon...\n\n"
 php-fpm --daemonize
 
 printf "Starting Nginx...\n\n"
-set -e
-
-if [[ "$1" == -* ]]; then
-    set -- nginx -g daemon off; "$@"
-fi
-
-exec "$@"
+service nginx start 
+ 
