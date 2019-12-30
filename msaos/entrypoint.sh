97b86255b575a81ddfccc333cba31ec073be21fc
kims man <zero12a@gmail.com>
2019-12-31 05:57:34 +0900

    entry

--- msaos/entrypoint.sh
+++ msaos/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd /data/www/oauth2-demo-php/web
+cd /data/www/o.s/oauth2-demo-php/web
 
-printf "\n\nStarting PHP daemon...\n\n"
+printf "\n\nStarting PHP client daemon...\n\n"
 
-php -S 0.0.0.0:8080 & php -S 0.0.0.0:8081
+php -S 0.0.0.0:8080 & 
+
+printf "\n\nStarting PHP server daemon...\n\n"
+php -S 0.0.0.0:8081
 #php -S 0.0.0.0:8081 -t /data/www/oauth2-demo-php/web
