484b3195ba3b986ff994fe7e7831b2bc12288b43
kims man <zero12a@gmail.com>
2020-03-16 07:52:03 +0900

    1

--- msads/entrypoint.sh
+++ msads/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-printf "\n\nStarting crontab...\n\n"
-crond -f &
 
 printf "\n\nStarting PHP daemon...\n\n"
 php-fpm --daemonize
 
-printf "Starting Nginx...\n\n"
-/etc/init.d/nginx start
+printf "\n\nStarting crontab...\n\n"
+crond -f 
