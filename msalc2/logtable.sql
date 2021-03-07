8a4ec83b8bd17c8c46c3176660a154b3722a07c3
kims man <zero12a@gmail.com>
2021-03-07 23:06:04 +0900

    .

---
+++ msalc2/logtable.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 172.17.0.1:3306
+-- 생성 시간: 21-03-06 01:40
+-- 서버 버전: 5.7.20
+-- PHP 버전: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `RSYSLOG`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `SystemEvents`
+--
+
+CREATE TABLE `SystemEvents` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `CustomerID` bigint(20) DEFAULT NULL,
+  `ReceivedAt` datetime DEFAULT NULL,
+  `DeviceReportedTime` datetime DEFAULT NULL,
+  `Facility` smallint(6) DEFAULT NULL,
+  `Priority` smallint(6) DEFAULT NULL,
+  `FromHost` varchar(60) DEFAULT NULL,
+  `Message` text,
+  `NTSeverity` int(11) DEFAULT NULL,
+  `Importance` int(11) DEFAULT NULL,
+  `EventSource` varchar(60) DEFAULT NULL,
+  `EventUser` varchar(60) DEFAULT NULL,
+  `EventCategory` int(11) DEFAULT NULL,
+  `EventID` int(11) DEFAULT NULL,
+  `EventBinaryData` text,
+  `MaxAvailable` int(11) DEFAULT NULL,
+  `CurrUsage` int(11) DEFAULT NULL,
+  `MinUsage` int(11) DEFAULT NULL,
+  `MaxUsage` int(11) DEFAULT NULL,
+  `InfoUnitID` int(11) DEFAULT NULL,
+  `SysLogTag` varchar(60) DEFAULT NULL,
+  `EventLogType` varchar(60) DEFAULT NULL,
+  `GenericFileName` varchar(60) DEFAULT NULL,
+  `SystemID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `SystemEvents`
+--
+ALTER TABLE `SystemEvents`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `SystemEvents`
+--
+ALTER TABLE `SystemEvents`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
